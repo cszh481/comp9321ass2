90028d6087ef83d9f7b9e38ef698d26f08413b85
cszh481 <NewFolder@192-168-1-3.tpgi.com.au>
2015-09-11 22:00:35 +1000

    comments

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -5,12 +5,15 @@ import dto.User;
 public interface UserDao {
 
 	/**
+	 * register a new user if user.getID ==0 otherwise update changes made to
+	 * input user
 	 * 
 	 * @param user
 	 */
 	public void saveOrUpdate(User user);
 
 	/**
+	 * get a user by username and password
 	 * 
 	 * @param username
 	 * @param password
@@ -18,9 +21,21 @@ public interface UserDao {
 	 */
 	public User getLoginUser(String username, String password);
 
-
+	/**
+	 * get user by username
+	 * 
+	 * @param username
+	 * @return
+	 */
 	public User getUserByUsername(String username);
 
+	/**
+	 * get user by uuid, this should only be used in email verification related
+	 * operations
+	 * 
+	 * @param uuid
+	 * @return
+	 */
 	public User getUserByUuid(String uuid);
 
 }

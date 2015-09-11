69fccffcd5c8ad269b2bdebbc809cfc0351fed67
cszh481 <NewFolder@fengrenyuan.local>
2015-09-11 20:01:10 +1000

    new unit test
    added unit test
    register works

---
+++ src/dao/test/UserDaoTest.java
@@ -0,0 +1,27 @@
+package dao.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import dao.UserDao;
+import dao.UserDaoImpl;
+import dto.User;
+
+public class UserDaoTest {
+
+	@Test
+	public void testSaveUser() {
+		UserDao userDao = UserDaoImpl.getInstance();
+//		User user = new User();
+//		user.setUsername("lee");
+//		user.setPassword("lee");
+//		userDao.saveOrUpdate(user);
+
+		User user2 = userDao.getUserByUsername("lee");
+
+		assertEquals("lee", user2.getUsername());
+
+	}
+
+}
